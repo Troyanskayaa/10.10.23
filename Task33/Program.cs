dee3137025e486aa610017f2c0d44bd0b9c42fff
Troyanskaya <stadniklena5@icloud.com>
2023-10-13 21:43:31 +0300

    Задание 33

---
+++ Task33/Program.cs
@@ -0,0 +1,49 @@
+﻿// Задача 33: Задайте массив. Напишите программу, которая
+// определяет, присутствует ли заданное число в массиве.
+// 4; массив [6, 7, 19, 345, 3] -> нет
+// -3; массив [6, 7, 19, 345, 3] -> да
+
+int[] CreateArrayRindInt(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+
+    for (int i=0; i < size; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+     Console.Write("[");
+    for (int i=0; i < arr.Length; i++)
+    {
+        if(i < arr.Length -1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    } 
+    Console.WriteLine("]");
+}
+
+bool FindNumber (int[] arr, int num)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(arr[i] == num)
+        {
+            return true;
+        }
+
+    }
+    return false;
+
+} 
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int[] array = CreateArrayRindInt(12, -9, 9);
+PrintArray(array);
+
+bool findNumber = FindNumber(array, 4);
+Console.WriteLine(findNumber ? "Число найдено" : "Число не найдено");
