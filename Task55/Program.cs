db3781110f192398377dffc4555013a05824e87c
Troyanskaya <stadniklena5@icloud.com>
2023-10-27 00:17:55 +0300

    Задание 55

---
+++ Task55/Program.cs
@@ -0,0 +1,57 @@
+﻿// Задача 55:
+// Задайте двумерный массив. Напишите программу,
+// которая заменяет строки на столбцы. В случае, если это
+// невозможно, программа должна вывести сообщение для
+// пользователя.
+
+int[,] CreatMatrixRandomInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new();
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write("{0}\t", matrix[i, j]);
+        }
+        Console.WriteLine();
+    }
+}
+
+
+void ReplaceRowsWithColomns(int[,] matrix)
+{
+    for (int i = 1; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            int temp = matrix[i, j];
+            matrix[i, j] = matrix[j, i];
+            matrix[j, i] = temp;
+        }
+    }
+}
+
+int[,] rndMatrix = CreatMatrixRandomInt(4, 4, 1, 9);
+PrintArray(rndMatrix);
+
+Console.WriteLine();
+if (rndMatrix.GetLength(0) != rndMatrix.GetLength(1))
+{
+Console.WriteLine("Невозможно заменить строки на столюцы!");
+return;
+}
+ReplaceRowsWithColomns(rndMatrix);
+PrintArray(rndMatrix);
