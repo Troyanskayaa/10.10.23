162159aaacd943212431f579190c98b96e24afc5
Troyanskaya <stadniklena5@icloud.com>
2023-10-29 01:52:02 +0300

    Задание 54

---
+++ Task54/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая
+// упорядочит по убыванию элементы каждой строки двумерного массива.
+
+
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(1, 10);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($" {matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void ReplaceFirstLastRow(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            int temp = matrix[i, j + 1];
+            matrix[i, j + 1] = matrix[i, j];
+            matrix[i, j] = temp;
+        }
+    }
+}
+
+
+int[,] array2d = CreateMatrixRndInt(3, 4, 1, 10);
+Console.WriteLine();
+ReplaceFirstLastRow(array2d);
+PrintMatrix(array2d);
